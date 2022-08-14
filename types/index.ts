bc315269f7a6ed788f76f1882fea0583bf3bfd90
Konstantin Timoshenko <konstantin.t@deovr.com>
2022-08-14 17:32:11 +0300

    migrate: Buffer decoders and tools

--- types/index.ts
+++ types/index.ts
@@ -1,3 +1,4 @@
+
 declare class AudioWorkletProcessor {
     public readonly port: MessagePort;
     public process(input: Float32Array[][], output: Float32Array[][], params: AudioParamMap): boolean;
@@ -9,4 +10,22 @@ declare interface AudioWorkletGlobalScope {
     readonly sampleRate: number;
 
     registerProcessor(name: string, ctor: any);
+}
+
+
+declare interface Envelope {
+    Get(point: number): number;
+}
+
+declare interface Sample {
+    
+}
+
+declare interface Instrument {
+    name: string;
+    number: number;
+    samplemap: Uint8Array | null;
+    samples: Sample[] | null;
+    env_vol?: Envelope;
+    env_pan?: Envelope;
 }
